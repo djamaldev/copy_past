6570cfc1ddb7ad16053b07e1d60cb288be34087b
djamaldev <34842155+djamaldev@users.noreply.github.com>
2022-02-24 16:12:30 +0100

    second release

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,14 +12,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController field = TextEditingController();
+  final List<String> _copiedText = [];
+  List<String> allCopiedText = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getData();
+    //_remove();
+  }
+
+  _getData() async {
+    //_getDataFromShared();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //allCopiedText.addAll(prefs.getStringList('copy')!);
+    Clipboard.getData(Clipboard.kTextPlain).then(
+      (value) {
+        _copiedText.addAll([value!.text!]);
+        for (final element in _copiedText) {
+          allCopiedText.insert(0, element);
+        }
+        //allCopiedText.insertAll(0, _copiedText);
+        //prefs.setStringList('copy', allCopiedText);
+        print(allCopiedText);
+      },
+    );
+  }
+
+  _remove() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //prefs.remove('copy');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('copy && past'),
-        ),
-        body:
-            Container() // This trailing comma makes auto-formatting nicer for build methods.
-        );
+        body: ListView.builder(
+            shrinkWrap: true,
+            itemCount: allCopiedText.length,
+            itemBuilder: (context, index) {
+              //_getData();
+              return ListTile(
+                title: Text(
+                  allCopiedText[index],
+                ),
+              );
+            }));
   }
 }
